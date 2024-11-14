d444a18a77d6c89dad14b9a27dcd0775a432a628
tuan0602 <anhtuan060201@gmail.com>
2024-11-14 14:16:58 +0700

    remove /publuc/css folder

--- src/index.js
+++ src/index.js
@@ -8,18 +8,25 @@ const hdb = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+console.log('path: ', path.join(__dirname, 'public'))
 //HTTP logger
 app.use(morgan('combined'));
 
 //Template engine
-app.engine('hdb', hdb.engine({extname: '.hdb'}));
-app.set('view engine', 'hdb');
-app.set('views', path.join(__dirname, 'resource\\views'));
+app.engine('hbs', hdb.engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 // console.log('PATH: ',path.join(__dirname, 'resources/views'))
+
+
 app.get('/', (req, res) => {
     res.render('home');
 })
 
+app.get('/news', (req, res) => {
+  res.render('News');
+})
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
